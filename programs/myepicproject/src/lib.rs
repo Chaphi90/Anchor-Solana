b80a3dfb725012f855a72c7f9f33614c01848421
Chantel Phillips <chaphi90@chantels-air.lan>
2022-09-29 19:24:30 -0400

    Retrieve Account Data

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -7,14 +7,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
-    let base_account = &mut ctx.base_account;
+    let base_account = &mut ctx.accounts.base_account;
     base_account.total_gifs = 0;
     Ok(())
   }
 }
 
 #[derive(Accounts)]
-pub struct StartStuffOff<'info'> {
+pub struct StartStuffOff<'info> {
     #[account(init, payer = user, space = 9000)]
     pub base_account: Account<'info, BaseAccount>,
     #[account(mut)]
